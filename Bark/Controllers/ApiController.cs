18bf3dd957c53a6de7dff502a82afabca2bf3a63
Bobbie <bobbiejgunnels@gmail.com>
2020-06-30 14:43:30 -0500

    Add API end point

--- Bark/Controllers/ApiController.cs
+++ Bark/Controllers/ApiController.cs
@@ -2,18 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 
+using Microsoft.AspNetCore.Mvc;
+using Dapper;
+
+using Bark.Models;
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Bark.Controllers
 {
     public class ApiController : Controller
     {
+       private readonly IBreedRepository repo;
+
+        public ApiController(IBreedRepository repo)
+        {
+            this.repo = repo;
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            var breeds = repo.GetAllBreeds();
+
+            return Json(new { success = true, breeds = breeds });
         }
     }
 }
