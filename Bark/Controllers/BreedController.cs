beda3c6b41a0b58b2b4b0be1722990a1ff29bb82
Bobbie <bobbiejgunnels@gmail.com>
2020-06-30 14:43:18 -0500

    Remove useless deps, refine CRUD and UI

--- Bark/Controllers/BreedController.cs
+++ Bark/Controllers/BreedController.cs
@@ -1,18 +1,17 @@
 ﻿using System;
-using Microsoft.AspNetCore.Mvc;
-using Bark.Models;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
 using Dapper;
 
-
+using Bark.Models;
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Bark.Controllers
 {
     public class BreedController : Controller  
-
     {
         private readonly IBreedRepository repo;
 
